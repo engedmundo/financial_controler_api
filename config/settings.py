feef74f280cf3ba633160564294e89ddc0884e0f
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-12-15 14:58:12 -0300

    update vercel json

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", '.vercel.app']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
