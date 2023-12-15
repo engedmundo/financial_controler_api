ef3e2115a91d13a96183dbdf1f7b538fa0b842c0
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-12-15 14:47:43 -0300

    update settings

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from decouple import config
@@ -101,6 +102,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = "static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
