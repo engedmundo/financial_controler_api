5ba85728dd127cf3c5a75dcd339c9f6712b136ca
Edmundo Abreu e Lima <engedmundo@gmail.com>
2023-12-15 18:04:59 -0300

    adjust filters

--- apps/financial_manager/admin/budget_admin.py
+++ apps/financial_manager/admin/budget_admin.py
@@ -7,15 +7,16 @@ from apps.financial_manager.models import Budget
 @admin.register(Budget)
 class BudgetAdmin(admin.ModelAdmin):
     list_display = [
+        "description",
         "category",
         "amount",
         "month",
         "year",
         "type",
-        "description",
     ]
 
     list_filter = (
         "month",
         "year",
+        "category",
     )
