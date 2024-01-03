ce990aec5efd682664534ee76a6eea483c78fd2e
Edmundo Abreu e Lima <engedmundo@gmail.com>
2024-01-03 17:58:00 -0300

    feature/create tests for financial manager models

---
+++ apps/financial_manager/tests/models/category_tests.py
@@ -0,0 +1,50 @@
+import pytest
+
+from apps.financial_manager.models import Category
+from apps.financial_manager.tests.factories import CategoryFactory
+from apps.core.tests.base_test import BaseModelsTest
+
+
+class CategoryModelTests(BaseModelsTest):
+    def test_create_category_model_instance(self):
+        # Given
+        expected_attrs = [
+            "user",
+            "name",
+            "description",
+            "created_at",
+            "updated_at",
+            "id",
+        ]
+
+        # When
+        category = CategoryFactory()
+        db_categories = Category.objects.all()
+        db_category = db_categories.first()
+
+        # Then
+        self.assertIsInstance(category, Category)
+        self.assertEqual(len(db_categories), 1)
+        self.assertEqual(str(db_category), category.name)
+
+        for attr_name in expected_attrs:
+            with self.subTest(attr=attr_name):
+                self.assertHasAttr(category, attr_name)
+                category_attr = getattr(category, attr_name)
+                db_category_attr = getattr(db_category, attr_name)
+                self.assertEqual(category_attr, db_category_attr)
+
+    def test_category_model_meta_verbose_names(self):
+        # Then
+        self.assertEqual(Category._meta.verbose_name, "Categoria")
+        self.assertEqual(Category._meta.verbose_name_plural, "Categorias")
+
+
+@pytest.mark.parametrize("attr_name", ["user", "name"])
+def test_create_category_raise_exception_without_required_fields(attr_name):
+    # Given
+    test_data = {attr_name: None}
+
+    # Then
+    with CategoryModelTests().assertRaises(Exception):
+        Category.objects.create(**test_data)
---
+++ apps/financial_manager/tests/models/transactions_tests.py
@@ -0,0 +1,69 @@
+import pytest
+
+from apps.account_manager.tests.factories import AccountFactory, CreditCardFactory
+from apps.core.tests.base_test import BaseModelsTest
+from apps.financial_manager.models import Transaction
+from apps.financial_manager.tests.factories import CategoryFactory, TransactionFactory
+
+
+class TransactionModelTests(BaseModelsTest):
+    def test_create_transaction_model_instance(self):
+        # Given
+        expected_attrs = [
+            "user",
+            "account",
+            "credit_card",
+            "category",
+            "amount",
+            "date",
+            "description",
+            "created_at",
+            "updated_at",
+            "id",
+        ]
+
+        # When
+        category = CategoryFactory()
+        account = AccountFactory(user=category.user)
+        card = CreditCardFactory(user=category.user)
+        transaction = TransactionFactory(
+            user=category.user,
+            account=account,
+            credit_card=card,
+            category=category,
+        )
+        db_transactions = Transaction.objects.all()
+        db_transaction = db_transactions.first()
+
+        # Then
+        self.assertIsInstance(transaction, Transaction)
+        self.assertEqual(len(db_transactions), 1)
+        self.assertEqual(str(db_transaction), transaction.description)
+
+        for attr_name in expected_attrs:
+            with self.subTest(attr=attr_name):
+                self.assertHasAttr(transaction, attr_name)
+                transaction_attr = getattr(transaction, attr_name)
+                db_transaction_attr = getattr(db_transaction, attr_name)
+
+                if attr_name == "date":
+                    db_transaction_attr = db_transaction_attr.strftime("%Y-%m-%d")
+
+                self.assertEqual(transaction_attr, db_transaction_attr)
+
+    def test_transaction_model_meta_verbose_names(self):
+        # Then
+        self.assertEqual(Transaction._meta.verbose_name, "Transação")
+        self.assertEqual(Transaction._meta.verbose_name_plural, "Transações")
+
+
+@pytest.mark.parametrize(
+    "attr_name", ["user", "category", "amount", "date", "type", "description"]
+)
+def test_create_transaction_raise_exception_without_required_fields(attr_name):
+    # Given
+    test_data = {attr_name: None}
+
+    # Then
+    with TransactionModelTests().assertRaises(Exception):
+        Transaction.objects.create(**test_data)
